53f823f80955bf13fdc04984f93bee002e01f9fc
Pjdaze <pjdazeux@gmail.com>
2020-02-17 21:42:44 -0500

    stable

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,52 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res) {
+  res.render("index");
+});
+
+router.get("/:time", (req, res) => {
+  const unixToNatural = unix => {
+    let date = new Date(unix * 100);
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    let month = months[date.getMonth()];
+    let day = date.getDate();
+    let year = date.getFullYear();
+
+    let result = `${month} ${day} ${year}`;
+    return result;
+  };
+
+  if (!isNaN(req.params.time)) {
+    let result = unixToNatural(req.params.time);
+
+    let data = { unix: req.params.time, natural: result };
+
+    res.json(data);
+  } else {
+    let natural = new Date(req.params.time);
+    if (!isNaN(natural)) {
+      let unix = natural / 1000;
+      let data = { unix, natural: req.params.time };
+      res.json(data);
+    } else {
+      res.json({ unix: null, natural: null });
+    }
+  }
 });
 
 module.exports = router;
