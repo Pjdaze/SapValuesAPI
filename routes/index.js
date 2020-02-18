00a914a20ac964a89fb7b46505ac4665bb5effd5
Pjdaze <pjdazeux@gmail.com>
2020-02-17 22:51:07 -0500

    stable

--- routes/index.js
+++ routes/index.js
@@ -6,46 +6,291 @@ router.get("/", function(req, res) {
   res.render("index");
 });
 
-router.get("/:time", (req, res) => {
-  const unixToNatural = unix => {
-    let date = new Date(unix * 100);
-    let months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December"
+router.get("/:values", (req, res) => {
+  if (!isNaN(req.params.values)) {
+    let data = [
+      {
+        label: "Almond Oil",
+        value: { LyeSapValue: 0.1367, PotassiumSapValue: 0.1925 }
+      },
+      {
+        label: "Aloe Vera Butter",
+        value: { LyeSapValue: 0.1788, PotassiumSapValue: 0.2518 }
+      },
+      {
+        label: "Aloe Vera Oil",
+        value: { LyeSapValue: 0.1421, PotassiumSapValue: 0.2001 }
+      },
+      {
+        label: "Apricot Kernel Oil",
+        value: { LyeSapValue: 0.1378, PotassiumSapValue: 0.1941 }
+      },
+      {
+        label: "Avocado Butter   ",
+        value: { LyeSapValue: 0.1339, PotassiumSapValue: 0.1886 }
+      },
+      {
+        label: "Avocado Oil",
+        value: { LyeSapValue: 0.1337, PotassiumSapValue: 0.1883 }
+      },
+      {
+        label: "Babassu Nut Oil",
+        value: { LyeSapValue: 0.1749, PotassiumSapValue: 0.2463 }
+      },
+      {
+        label: "Beeswax Animal   ",
+        value: { LyeSapValue: 0.0689, PotassiumSapValue: 0.097 }
+      },
+      {
+        label: "Borage Oil",
+        value: { LyeSapValue: 0.1339, PotassiumSapValue: 0.1886 }
+      },
+      {
+        label: "Candelilla Wax",
+        value: { LyeSapValue: 0.0322, PotassiumSapValue: 0.0454 }
+      },
+      {
+        label: "Canola Oil",
+        value: { LyeSapValue: 0.1328, PotassiumSapValue: 0.187 }
+      },
+
+      {
+        label: "Castor Bean Oil",
+        value: { LyeSapValue: 0.1286, PotassiumSapValue: 0.1811 }
+      },
+      {
+        label: "Cherry Kernel Oil",
+        value: { LyeSapValue: 0.1389, PotassiumSapValue: 0.1956 }
+      },
+      {
+        label: "Chicken Fat",
+        value: { LyeSapValue: 0.1356, PotassiumSapValue: 0.191 }
+      },
+      {
+        label: "Cocoa Butter",
+        value: { LyeSapValue: 0.1378, PotassiumSapValue: 0.1941 }
+      },
+      {
+        label: "Coconut Oil, Refined 76",
+        value: { LyeSapValue: 0.191, PotassiumSapValue: 0.269 }
+      },
+      {
+        label: "Coconut Oil, Hydrogenated 92",
+        value: { LyeSapValue: 0.191, PotassiumSapValue: 0.269 }
+      },
+      {
+        label: "Coconut Oil, Fractionated/Saturated",
+        value: { LyeSapValue: 0.2321, PotassiumSapValue: 0.3269 }
+      },
+      {
+        label: "Corn Oil",
+        value: { LyeSapValue: 0.1368, PotassiumSapValue: 0.1927 }
+      },
+      {
+        label: "Cottonseed Oil",
+        value: { LyeSapValue: 0.1387, PotassiumSapValue: 0.1954 }
+      },
+      {
+        label: "Copha Vegetable Shortening",
+        value: { LyeSapValue: 0.191, PotassiumSapValue: 0.269 }
+      },
+      {
+        label: "Crisco Vegetable Shortening",
+        value: { LyeSapValue: 0.1369, PotassiumSapValue: 0.1928 }
+      },
+      {
+        label: "Emu Oil Animal",
+        value: { LyeSapValue: 0.1377, PotassiumSapValue: 0.1939 }
+      },
+      {
+        label: "Evening Primrose Oil",
+        value: { LyeSapValue: 0.1362, PotassiumSapValue: 0.1918 }
+      },
+      {
+        label: "Flaxseed Oil",
+        value: { LyeSapValue: 0.1358, PotassiumSapValue: 0.1913 }
+      },
+      {
+        label: "Goat Fat",
+        value: { LyeSapValue: 0.1382, PotassiumSapValue: 0.1946 }
+      },
+      {
+        label: "Goose Fat",
+        value: { LyeSapValue: 0.1349, PotassiumSapValue: 0.19 }
+      },
+      {
+        label: "Grapeseed Oil ",
+        value: { LyeSapValue: 0.1321, PotassiumSapValue: 0.1861 }
+      },
+      {
+        label: "Hazelnut Oil ",
+        value: { LyeSapValue: 0.1369, PotassiumSapValue: 0.1928 }
+      },
+      {
+        label: "Hempseed Oil",
+        value: { LyeSapValue: 0.1359, PotassiumSapValue: 0.1914 }
+      },
+      {
+        label: "Jojoba Seed Oil ",
+        value: { LyeSapValue: 0.0695, PotassiumSapValue: 0.0979 }
+      },
+      {
+        label: "Jojoba Seed Liquid Wax",
+        value: { LyeSapValue: 0.0695, PotassiumSapValue: 0.0979 }
+      },
+      {
+        label: "Karite Butter",
+        LyeSapValue: 0.1296,
+        PotassiumSapValue: 0.1825
+      },
+      {
+        label: "Kremelta Vegetable Shortening",
+        LyeSapValue: 0.191,
+        PotassiumSapValue: 0.269
+      },
+      {
+        label: "Kukui Nut Oil",
+        value: { LyeSapValue: 0.1351, PotassiumSapValue: 0.1903 }
+      },
+      {
+        label: "Lanolin Animal",
+        value: { LyeSapValue: 0.1399, PotassiumSapValue: 0.197 }
+      },
+      {
+        label: "Lard Animal",
+        value: { LyeSapValue: 0.1358, PotassiumSapValue: 0.1913 }
+      },
+      {
+        label: "Linseed Oil",
+        value: { LyeSapValue: 0.1391, PotassiumSapValue: 0.1959 }
+      },
+      {
+        label: "Macadamia Nut Oil ",
+        LyeSapValue: 0.1599,
+        PotassiumSapValue: 0.2252
+      },
+      {
+        label: "Milk Fat",
+        value: { LyeSapValue: 0.1403, PotassiumSapValue: 0.1976 }
+      },
+      {
+        label: "Mink Oil",
+        value: { LyeSapValue: 0.1796, PotassiumSapValue: 0.253 }
+      },
+      {
+        label: "Neem Tree Oil",
+        value: { LyeSapValue: 0.1372, PotassiumSapValue: 0.1932 }
+      },
+      {
+        label: "Olive Oil ",
+        value: { LyeSapValue: 0.1353, PotassiumSapValue: 0.1906 }
+      },
+      {
+        label: "Ostrich Oil ",
+        value: { LyeSapValue: 0.1385, PotassiumSapValue: 0.1951 }
+      },
+      {
+        label: "Palm Kernel Oil ",
+        value: { LyeSapValue: 0.1777, PotassiumSapValue: 0.2503 }
+      },
+      {
+        label: "Palm Oil ",
+        value: { LyeSapValue: 0.142, PotassiumSapValue: 0.2 }
+      },
+      {
+        label: "Peach Kernel Oil",
+        value: { LyeSapValue: 0.1361, PotassiumSapValue: 0.1917 }
+      },
+      {
+        label: "Peanut Oil",
+        value: { LyeSapValue: 0.1367, PotassiumSapValue: 0.1925 }
+      },
+      {
+        label: "Pine Rosin",
+        value: { LyeSapValue: 0.1298, PotassiumSapValue: 0.182 }
+      },
+      {
+        label: "Pumpkin Seed Oil",
+        value: { LyeSapValue: 0.1389, PotassiumSapValue: 0.1956 }
+      },
+      {
+        label: "Rapeseed Oil",
+        value: { LyeSapValue: 0.1328, PotassiumSapValue: 0.187 }
+      },
+      {
+        label: "Rice Bran Oil",
+        value: { LyeSapValue: 0.1284, PotassiumSapValue: 0.1808 }
+      },
+      {
+        label: "Safflower Oil, High Linoleic Acid",
+        value: { LyeSapValue: 0.1374, PotassiumSapValue: 0.1935 }
+      },
+      {
+        label: "Safflower Oil, High Oleic Acid ",
+        value: { LyeSapValue: 0.1369, PotassiumSapValue: 0.1928 }
+      },
+      {
+        label: "Sesame Seed Oil ",
+        value: { LyeSapValue: 0.1336, PotassiumSapValue: 0.1882 }
+      },
+      {
+        label: "Shea Butter",
+        value: { LyeSapValue: 0.1296, PotassiumSapValue: 0.1825 }
+      },
+      {
+        label: "Soybean Oil",
+        value: { LyeSapValue: 0.1359, PotassiumSapValue: 0.1914 }
+      },
+      {
+        label: "Soybean Oil, 27.5% Hydrogenated",
+        value: { LyeSapValue: 0.1361, PotassiumSapValue: 0.1917 }
+      },
+      {
+        label: "Stearic Acid, Animal-Source",
+        value: { LyeSapValue: 0.1413, PotassiumSapValue: 0.199 }
+      },
+      {
+        label: "Stearic Acid, Vegetable-Source",
+        value: { LyeSapValue: 0.1411, PotassiumSapValue: 0.1987 }
+      },
+      {
+        label: "Sunflower Seed Oil",
+        value: { LyeSapValue: 0.1358, PotassiumSapValue: 0.1913 }
+      },
+      {
+        label: "Sunflower Seed Oil, High Oleic Acid",
+        value: { LyeSapValue: 0.1351, PotassiumSapValue: 0.1903 }
+      },
+      {
+        label: "Tallow, Deer ",
+        value: { LyeSapValue: 0.1382, PotassiumSapValue: 0.1946 }
+      },
+      {
+        label: "Tallow, Sheep ",
+        value: { LyeSapValue: 0.1384, PotassiumSapValue: 0.1949 }
+      },
+      {
+        label: "Tallow, Beef ",
+        value: { LyeSapValue: 0.1419, PotassiumSapValue: 0.1999 }
+      },
+      {
+        label: "Tamanu Seed Oil",
+        value: { LyeSapValue: 0.1437, PotassiumSapValue: 0.2024 }
+      },
+      {
+        label: "Tiaré Flower Oil ",
+        value: { LyeSapValue: 0.1796, PotassiumSapValue: 0.253 }
+      },
+      {
+        label: "Walnut Oil ",
+        value: { LyeSapValue: 0.1349, PotassiumSapValue: 0.19 }
+      },
+      {
+        label: "Wheat Germ Oil ",
+        value: { LyeSapValue: 0.1319, PotassiumSapValue: 0.1858 }
+      }
     ];
-    let month = months[date.getMonth()];
-    let day = date.getDate();
-    let year = date.getFullYear();
-
-    let result = `${month} ${day} ${year}`;
-    return result;
-  };
-
-  if (!isNaN(req.params.time)) {
-    let result = unixToNatural(req.params.time);
-
-    let data = { unix: req.params.time, natural: result };
-
     res.json(data);
-  } else {
-    let natural = new Date(req.params.time);
-    if (!isNaN(natural)) {
-      let unix = natural / 1000;
-      let data = { unix, natural: req.params.time };
-      res.json(data);
-    } else {
-      res.json({ unix: null, natural: null });
-    }
   }
 });
 
