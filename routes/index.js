0e910dae67a548f7a21dccc8574d49114642cad3
Pjdaze <pjdazeux@gmail.com>
2020-02-18 01:39:23 -0500

    s

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.render("index");
 });
 
